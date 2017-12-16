03f920a27b1820abd4ae1e11f08f6a94cd3f1e9a
arconom <arconom@hotmail.com>
2017-12-16 10:32:31 -0500

    VectorCalculator should be ready for use.

--- Assets/Controllers/VectorCalculator.cs
+++ Assets/Controllers/VectorCalculator.cs
@@ -13,14 +13,34 @@ namespace Assets.Controllers
         /// <returns></returns>
         public float GetDistance(float latitudeA, float longitudeA, float latitudeB, float longitudeB)
         {
-            float dLat = RadiansToDegrees(latitudeB) - RadiansToDegrees(latitudeA);
-            float dLon = RadiansToDegrees(longitudeB) - RadiansToDegrees(longitudeA);
-            float a = Mathf.Sin(dLat / 2) * Mathf.Sin(dLat / 2) +
-            Mathf.Cos(RadiansToDegrees(latitudeA)) * Mathf.Cos(RadiansToDegrees(latitudeB)) *
-            Mathf.Sin(dLon / 2) * Mathf.Sin(dLon / 2);
-            float c = 2 * Mathf.Atan2(Mathf.Sqrt(a), Mathf.Sqrt(1 - a));
+            float dLat = DegreesToRadians(latitudeB) - DegreesToRadians(latitudeA);
+            float dLon = DegreesToRadians(longitudeB) - DegreesToRadians(longitudeA);
+            float a = Mathf.Sin(dLat / 2) * Mathf.Sin(dLat / 2)
+                + Mathf.Cos(DegreesToRadians(latitudeA))
+                * Mathf.Cos(DegreesToRadians(latitudeB))
+                * Mathf.Sin(dLon / 2)
+                * Mathf.Sin(dLon / 2);
+            float c = (float)(2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a)));
             float d = Constants.Constants.EarthRadius * c;
             return d * 1000f; // meters
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="latitudeA"></param>
+        /// <param name="longitudeA"></param>
+        /// <param name="latitudeB"></param>
+        /// <param name="longitudeB"></param>
+        /// <returns></returns>
+        public float GetBearing(float latitudeA, float longitudeA, float latitudeB, float longitudeB)
+        {
+            float y = Mathf.Sin(DegreesToRadians(longitudeB - longitudeA));
+            float x = Mathf.Cos(DegreesToRadians(latitudeA)) * Mathf.Sin(DegreesToRadians(latitudeB))
+                - Mathf.Sin(DegreesToRadians(latitudeA)) * Mathf.Cos(DegreesToRadians(latitudeB))
+                * Mathf.Cos(DegreesToRadians(longitudeB - longitudeA));
+            float dLat = DegreesToRadians(latitudeB) - DegreesToRadians(latitudeA);
+            return RadiansToDegrees(Mathf.Atan2(y, x));
         }
 
         /// <summary>
@@ -28,9 +48,15 @@ namespace Assets.Controllers
         /// </summary>
         /// <param name="radians"></param>
         /// <returns></returns>
+        private float DegreesToRadians(float radians)
+        {
+            return radians * Mathf.PI / 180.0f;
+        }
+
+
         private float RadiansToDegrees(float radians)
         {
-            return radians * Mathf.PI / 180;
+            return radians * 180.0f / Mathf.PI;
         }
 
         /// <summary>
@@ -45,10 +71,49 @@ namespace Assets.Controllers
             return Mathf.Acos((Mathf.Pow(sideA, 2) + Mathf.Pow(sideB, 2) - Mathf.Pow(sideC, 2)) / (2 * sideA * sideB));
         }
 
-        private Vector3 calculateTransformPosition(float lat, float lon)
+        /// <summary>
+        /// Calculates the transform position for a waypoint based on its lat long and the cameras lat long
+        /// The objective is to place the waypoint a short distance from the camera in the same direction as the remote GPS
+        /// </summary>
+        /// <param name="lat">The lat.</param>
+        /// <param name="lon">The lon.</param>
+        /// <returns></returns>
+        public Vector3 getRelativePosition(float localLatitude, float localLongitude, float remoteLatitude, float remoteLongitude)
         {
-            throw new NotImplementedException();
+            float deltaLatitude = localLatitude - remoteLatitude;
+            float deltaLongitude = localLongitude - remoteLongitude;
+            Vector3 deltaPosition = new Vector3(LatitudeToMeters(deltaLatitude), 1, LongitudeToMeters(deltaLongitude));
+            Vector3 remotePosition = new Vector3(LatitudeToMeters(remoteLatitude), 1, LongitudeToMeters(remoteLongitude));
+
+            //we should use the current GPS to calculate the camera's position, but the demo has the camera starting at 0,0,0 for now.
+            Vector3 localPosition = new Vector3(LatitudeToMeters(localLatitude), 1, LongitudeToMeters(localLongitude));
+            Vector3 demoPosition = new Vector3(0, 1, 0);
+
+            return getPointOnLine(demoPosition, deltaPosition, 1f);
         }
+
+        /// <summary>
+        /// Calculate the number of meters per degree of latitude at a given latitude.
+        /// </summary>
+        /// <param name="degrees"></param>
+        /// <returns></returns>
+        private float LatitudeToMeters(float degrees)
+        {
+            return 111132.92f - 559.82f * Mathf.Cos(2 * degrees) + 1.175f * Mathf.Cos(4 * degrees) - 0.0023f * Mathf.Cos(6 * degrees);
+
+        }
+
+        /// <summary>
+        /// Calculate the number of meters per degree of longitude at a given longitude.
+        /// </summary>
+        /// <param name="degrees"></param>
+        /// <returns></returns>
+        private float LongitudeToMeters(float degrees)
+        {
+            return 111412.84f * Mathf.Cos(degrees) - 93.5f * Mathf.Cos(3 * degrees) + 0.118f * Mathf.Cos(5 * degrees);
+
+        }
+
 
         /// <summary>
         /// Gets the point on line.
@@ -62,7 +127,8 @@ namespace Assets.Controllers
             Vector3 vector = pointA - pointB;
             float vectorLength = Mathf.Sqrt(Mathf.Pow(vector.x, 2) + Mathf.Pow(vector.y, 2));
             Vector3 unitVector = vector / vectorLength;
-            return pointA + (unitVector * distance);
+            Vector3 returnMe = pointA + (unitVector * distance);
+            return returnMe;
         }
     }
 }
